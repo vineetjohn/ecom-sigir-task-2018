55763a48f92e384581b8b30203e2c4acc8ed51bf
Vineet John <iamvineetjohn@gmail.com>
2018-05-23 09:20:14 -0400

    Add taxonomy representation method and pickling

--- src/create_taxonomy.py
+++ src/create_taxonomy.py
@@ -1,28 +1,35 @@
 import argparse
 import logging
+import pickle
 import sys
 
 from src.config import global_config
-from src.utils import log_initializer
+from src.utils import logging_inferface, taxonomy_interface
 
 logger = logging.getLogger(global_config.logger_name)
 
 
 def generate_taxonomy(input_file_path, taxonomy_save_path):
+    taxonomy = taxonomy_interface.Taxonomy()
     with open(input_file_path) as input_file:
         for line in input_file:
             [_, category_string] = line.strip().split('\t')
             categories = category_string.strip().split('>')
-            logger.debug(categories)
+            taxonomy.add_categories(categories)
+
+    logger.debug(taxonomy)
+    with open(taxonomy_save_path, 'wb') as taxonomy_file:
+        pickle.dump(taxonomy, taxonomy_file)
+        logger.info("Saved taxonomy to {}".format(taxonomy_save_path))
 
 
 def main(args):
     global logger
-    logger = log_initializer.setup_custom_logger(global_config.logger_name, global_config.log_level)
+    logger = logging_inferface.setup_custom_logger(global_config.logger_name, global_config.log_level)
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--input-file-path", type=str, required=True)
-    parser.add_argument("--taxonomy-file-path", type=str)
+    parser.add_argument("--taxonomy-file-path", type=str, required=True)
     options = vars(parser.parse_args(args=args))
     logger.info("Arguments: {}".format(options))
 
---
+++ src/utils/taxonomy_interface.py
@@ -0,0 +1,32 @@
+class Taxonomy:
+    def __init__(self):
+        self.root = TaxonomyNode(-1)
+        self.nodes = dict()
+
+    def __repr__(self):
+        return str(self.root)
+
+    def add_categories(self, categories):
+        parent = self.root
+        for category in categories:
+            if not category in parent.children:
+                category_node = TaxonomyNode(category)
+                category_node.parent = parent
+                parent.children[category] = category_node
+            else:
+                category_node = parent.children[category]
+            parent = category_node
+
+
+class TaxonomyNode:
+    def __init__(self, id):
+        self.id = id
+        self.parent = None
+        self.children = dict()
+
+    def __repr__(self):
+        representation = "{}=(".format(self.id)
+        for child in self.children:
+            representation += "{}, ".format(self.children[child])
+        representation += ")"
+        return representation
